531ae8e5fa75fb5b5146ccd1d90631f3b941244e
Mingyu Jeong <jmg9212@gmail.com>
2023-07-07 16:02:35 +0900

    스프링

---
+++ src/main/java/com/test/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/test")
+	public String test() {
+		
+		return "test";
+	}
+	@RequestMapping("/main")
+	public String main() {
+		
+		return "index";
+	}
+	@RequestMapping("/signUp")
+	public String signup(){
+		
+		return "sign-up";
+	}
+	
+	@RequestMapping("/signIn")
+	public String signin(){
+		
+		return "sign-in";
+	}
+}
