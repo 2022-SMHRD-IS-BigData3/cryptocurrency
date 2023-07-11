867d58d93b9d7db013d07e6ee3d5784f9a5a1aad
Mingyu Jeong <jmg9212@gmail.com>
2023-07-11 16:49:59 +0900

    값 받아서 db저장

--- src/main/java/com/test/socket/CustomWebSocketHandler.java
+++ src/main/java/com/test/socket/CustomWebSocketHandler.java
@@ -1,7 +1,9 @@
-
 package com.test.socket;
 
-import java.time.Duration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -22,64 +24,84 @@ import reactor.core.publisher.Mono;
 public class CustomWebSocketHandler implements WebSocketHandler {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private final AtomicReference<Instant> lastProcessedInstant = new AtomicReference<>(Instant.MIN);
-    private final ZoneId desiredTimeZone = ZoneId.of("America/New_York").normalized(); // Specify the desired timezone here
+    private final AtomicReference<JsonNode> latestCandle = new AtomicReference<>();
+    private final ZoneId desiredTimeZone = ZoneId.of("Asia/Seoul").normalized();
+
+    // MySQL database connection details
+    private final String jdbcUrl = "jdbc:mysql://project-db-cgi.smhrd.com:3307/cgi_2_230701_1";
+    private final String username = "cgi_2_230701_1";
+    private final String password = "smhrd1";
 
     @Override
     public Mono<Void> handle(WebSocketSession session) {
-        Mono<Void> sendMono = session.send(Mono.just(session.textMessage("{\"method\": \"SUBSCRIBE\",\"params\": [\"btcusdt@kline_5m\"],\"id\": 1}")));
+        Mono<Void> sendMono = session.send(Mono.just(session.textMessage("{\"method\": \"SUBSCRIBE\",\"params\": [\"btcusdt@kline_1m\"],\"id\": 1}")));
         Flux<WebSocketMessage> receiveFlux = session.receive();
 
         return sendMono.thenMany(receiveFlux)
-                .concatMap(message -> processMessage(message, session))
+                .filter(this::isCandleMessage)
+                .flatMap(this::processMessage)
                 .then();
     }
 
-    private Mono<Void> processMessage(WebSocketMessage message, WebSocketSession session) {
-        return Mono.just(message.getPayloadAsText())
-                .filter(payload -> shouldProcessData())
-                .flatMap(payload -> {
-                    try {
-                        JsonNode jsonNode = objectMapper.readTree(payload);
-                        if (jsonNode.has("k")) {
-                            JsonNode klineNode = jsonNode.get("k");
-                            long startTime = klineNode.get("t").asLong();
-                            long closeTime = klineNode.get("T").asLong();
-                            String symbol = klineNode.get("s").asText();
-                            String interval = klineNode.get("i").asText();
-                            String openPrice = klineNode.get("o").asText();
-                            String closePrice = klineNode.get("c").asText();
-                            String highPrice = klineNode.get("h").asText();
-                            String lowPrice = klineNode.get("l").asText();
-                            LocalDateTime currentTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(closeTime), desiredTimeZone);
-
-                            System.out.println("Symbol: " + symbol);
-                            System.out.println("Interval: " + interval);
-                            System.out.println("Current Time : " + currentTime);
-                            System.out.println("Open Price: " + openPrice);
-                            System.out.println("Close Price: " + closePrice);
-                            System.out.println("High Price: " + highPrice);
-                            System.out.println("Low Price: " + lowPrice);
-
-                            updateLastProcessedInstant(Instant.ofEpochMilli(closeTime));
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    return Mono.empty();
-                })
-                .then();
+    private boolean isCandleMessage(WebSocketMessage message) {
+        String payload = message.getPayloadAsText();
+        try {
+            JsonNode jsonNode = objectMapper.readTree(payload);
+            return jsonNode.has("k") && jsonNode.get("k").has("x") && jsonNode.get("k").get("x").asBoolean();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
-    private void updateLastProcessedInstant(Instant instant) {
-        lastProcessedInstant.set(instant);
+    private Mono<Void> processMessage(WebSocketMessage message) {
+        String payload = message.getPayloadAsText();
+        try {
+            JsonNode jsonNode = objectMapper.readTree(payload);
+            if (jsonNode.has("k")) {
+                JsonNode klineNode = jsonNode.get("k");
+                latestCandle.set(klineNode);
+                saveCandleData(klineNode);
+                processCandle(klineNode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Mono.empty();
     }
 
-    private boolean shouldProcessData() {
-        Instant now = Instant.now();
-        Instant lastProcessed = lastProcessedInstant.get();
-        Duration duration = Duration.between(lastProcessed, now);
-        return duration.compareTo(Duration.ofMinutes(5)) >= 0;
+    private void saveCandleData(JsonNode klineNode) throws SQLException {
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+            long closeTime = klineNode.get("T").asLong();
+            double openPrice = klineNode.get("o").asDouble();
+            double closePrice = klineNode.get("c").asDouble();
+            double highPrice = klineNode.get("h").asDouble();
+            double lowPrice = klineNode.get("l").asDouble();
+            LocalDateTime currentTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(closeTime), desiredTimeZone);
+
+            // Prepare the SQL statement
+            String sql = "INSERT INTO tbl_minute5 ( m5_start, m5_end, m5_max, m5_min) VALUES (?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setDouble(1, openPrice);
+            statement.setDouble(2, closePrice);
+            statement.setDouble(3, highPrice);
+            statement.setDouble(4, lowPrice);
+
+            // Execute the SQL statement
+            statement.executeUpdate();
+        }
+    }
+
+
+    private void processCandle(JsonNode klineNode) {
+        double openPrice = klineNode.get("o").asDouble();
+        double closePrice = klineNode.get("c").asDouble();
+        double highPrice = klineNode.get("h").asDouble();
+        double lowPrice = klineNode.get("l").asDouble();
+
+        System.out.println("Open Price: " + openPrice);
+        System.out.println("Close Price: " + closePrice);
+        System.out.println("High Price: " + highPrice);
+        System.out.println("Low Price: " + lowPrice);
     }
 }
-
